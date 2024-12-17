79b8520c266807f8ce0e977a277f6276fd35f0f3
blevin04 <jamestonito04@gmail.com>
2024-12-17 10:04:07 +0300

    navigation

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:klust/inboxpage.dart';
+import 'package:klust/profilepage.dart';
+import 'package:klust/searchpage.dart';
+import 'package:klust/shopHome.dart';
 
+ValueNotifier<int> currentPage = ValueNotifier(0);
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
@@ -7,18 +12,42 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        leading:const CircleAvatar(),
+        leading:InkWell(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>const Profilepage()));
+          },
+          child: const CircleAvatar()),
         title:const Text("Klust"),
         actions: [
-          IconButton(onPressed: (){}, icon:const Icon(Icons.shopping_cart))
+          IconButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>const Shophome()));
+          }, icon:const Icon(Icons.shopping_cart))
         ],
+      ),
+      body: ListenableBuilder(
+        listenable:currentPage ,
+        builder: (BuildContext context, child) {
+          if (currentPage.value == 0) {
+            return const Homepage();
+          }
+          if (currentPage.value == 1) {
+            return const Searchpage();
+          }
+          return const Inboxpage();
+        },
       ),
       bottomNavigationBar: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.home)),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.message))
+          IconButton(onPressed: (){
+            currentPage.value = 0;
+          }, icon: const Icon(Icons.home)),
+          IconButton(onPressed: (){
+            currentPage.value = 1;
+          }, icon: const Icon(Icons.search)),
+          IconButton(onPressed: (){
+            currentPage.value = 2;
+          }, icon: const Icon(Icons.message))
         ],
       ),
     );
--- lib/shopHome.dart
+++ lib/shopHome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:klust/focusPage.dart';
 
 class Shophome extends StatefulWidget {
   const Shophome({super.key});
@@ -42,10 +43,16 @@ class _ShophomeState extends State<Shophome> {
               ),
               itemCount: 10,
               itemBuilder: (BuildContext context, int index) {
-                return Card();
+                return Card(
+                  child: InkWell(
+                    onTap: (){
+                      Navigator.push(context, (MaterialPageRoute(builder: (context)=>const Focuspage())));
+                    },
+                  ),
+                );
               },
             ),
-            
+
           ],
         ),
       ),
